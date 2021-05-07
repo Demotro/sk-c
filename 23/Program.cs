1e9d74cdbc3b6ef43ce6f754a886974a4e232632
Demotro <jirksamm@gmail.com>
2021-05-07 13:02:20 +0200

    sk#c

--- 23/Program.cs
+++ 23/Program.cs
@@ -7,26 +7,44 @@ namespace _23
     {
         static void Main(string[] args)
         {
-            string lokace1 = @"C:\Users\GAME PC\source\repos\23\Soubory\MujPrvniSoubor.txt";
-            string lokace = @"C:\Users\GAME PC\source\repos\23\Soubory\";
+            //Streamwriter
+            string lokace1 = @"C:\Users\krajc\source\repos\H23\Soubory\MujPrvniSoubor.txt";
+            string lokace = @"C:\Users\krajc\source\repos\H23\Soubory\";
             string nazevsouboru = "MujPrvniSoubor.txt";
             string[] radky = { "Prvni radek", "Druhy radek", "Treti radek" };
-            using (StreamWriter outputfile = new StreamWriter(Path.Combine(lokace, nazevsouboru), true))
+            using (StreamWriter outputfile = new StreamWriter(Path.Combine(lokace, nazevsouboru)))
             {
                 for (int i = 0; i < radky.Length; i++)
                 {
                     outputfile.WriteLine(radky[i]);
                 }
             }
-            string[] radky2 = { "Prvni radek", "Druhy radek", "Treti radek" };
+            string[] radky2 = { "Prvni1 radek", "Druhy1 radek", "Treti1 radek" };
+            //True only for append - true pouze pro pridani jiz existujicimu
             using (StreamWriter outputfile = new StreamWriter(Path.Combine(lokace, nazevsouboru), true))
             {
+
                 for (int i = 0; i < radky2.Length; i++)
                 {
                     outputfile.WriteLine(radky2[i]);
                 }
-            }
 
+            }
+            //Zapiste jako samostatny radek vas veskery vystup z konzole. Pokud bude prazdny string nebo 0, pak to ukoncete
+            string radek = "";
+            using (StreamWriter outputfile = new StreamWriter(Path.Combine(lokace, nazevsouboru), true))
+            {
+                while (true)
+                {
+                    Console.WriteLine("Zapisuj radek a potvrd Enter. Pokud chces skoncit, zadej 0 a potvrd enter");
+                    radek = Console.ReadLine();
+                    if (radek == "0")
+                    {
+                        break;
+                    }
+                    outputfile.WriteLine(radek);
+                }
+            }
         }
     }
 }
