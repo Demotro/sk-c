922180a5d3dbd5b190a68d461cc6656217184e42
Demotro <jirksamm@gmail.com>
2021-05-27 22:16:12 +0200

    sk#c

---
+++ 24/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+
+namespace _24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string lokaceAdr = @"C:\Users\GAME PC\source\repos\24\Output\";
+            string nazevsouboru = "vystupni.txt";
+
+            using (StreamWriter output = new StreamWriter(Path.Combine(lokaceAdr, nazevsouboru)))
+            {
+                //for cyklus s 10 hodnoty v souboru
+                for (int i = 0; i < 10; i++)
+                {
+                    if (i == 9)
+                    {
+                        output.Write(i + 1);
+                    }
+                    else
+                    {
+                        output.WriteLine(i + 1);
+                    }
+                }
+            }
+            /*
+            using (StreamWriter output = new StreamWriter(Path.Combine(lokaceAdr, nazevsouboru)))
+            {
+                string radek;
+                while (true)
+                {
+                    Console.WriteLine("Muzes psat jak o zivot, dokud nenapises STOP");
+                    radek = Console.ReadLine();
+                    if (radek == "STOP")
+                    {
+                        break;
+                    }
+                    output.WriteLine(radek);    
+                }
+            }
+            */
+            Random rnd = new Random();
+            string str = "soubor";
+            for (int i = 0; i < 5; i++)
+            {
+                int treshold = rnd.Next(0, 100);
+                string outputfile = str + (i + 1).ToString() + ".txt";
+                using (StreamWriter output = new StreamWriter(Path.Combine(lokaceAdr, outputfile)))
+                {
+                    for (int j = 0; j < treshold; j++)
+                    {
+                        output.WriteLine(rnd.Next(0,10));
+                    }
+                }
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                string outputfile = str + (i + 1).ToString() + ".txt";
+                using (StreamReader output = new StreamReader(Path.Combine(lokaceAdr, outputfile)))
+                {
+                    string radek;
+                    Console.WriteLine("Vtej v:" + (str + (i + 1)));
+                    while ((radek = output.ReadLine()) != null)
+                    {
+                        Console.WriteLine(radek);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 25/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+
+namespace _25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string lokace = @"C:\Users\GAME PC\source\repos\25\Files\";
+            /*
+            using (StreamWriter sw = new StreamWriter
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    sw.WriteLine(i + 1);
+                }
+            }
+
+            using (StreamReader sr = new StreamReader(Path.Combine(lokace, "soubor1.txt")))
+            {
+                string radek;
+                while ((radek = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(radek);
+                }
+            }
+
+            using (StreamWriter sw = new StreamWriter(Path.Combine(lokace, "soubor1.txt"), true))
+            {
+                for (int i = 10; i < 20; i++)
+                {
+                    sw.WriteLine(i + 1);
+                }
+            }
+
+            string data = "";
+            for (int i = 10; i < 20; i++)
+            {
+                data += (i + 1) + "\n";
+            }
+
+            using (StreamWriter sw = new StreamWriter(Path.Combine(lokace, "soubor1.txt"), false))
+            {
+                sw.WriteLine(data);
+            }
+            */
+
+            string data = "";
+            for (int i = 0; i < 10; i++)
+            {
+                data += (i + 1) + "\n";
+            }
+            //zapis(Path.Combine(lokace, "soubor1.txt"), data);
+            SouborJob(Path.Combine(lokace, "soubor1.txt"), "w", data);
+            data = "";
+            for (int i = 10; i < 20; i++)
+            {
+                data += (i + 1) + "\n";
+            }
+            SouborJob(Path.Combine(lokace, "soubor1.txt"), "a", data);
+            //zapis(Path.Combine(lokace, "soubor1.txt"), data, true);
+            string str = SouborJob(Path.Combine(lokace, "soubor1.txt"), "r");
+            Console.WriteLine(str);
+        }
+        static void zapis(string lokace, string data, bool pridej = false)
+        {
+            using (StreamWriter sw = new StreamWriter(lokace, pridej))
+            {
+                sw.WriteLine(data);
+            }
+        }
+
+        static string SouborJob(string lokace, string delej, string data = "")
+        {
+            //Cteni
+            if (delej == "r")
+            {
+                string output = "";
+                using (StreamReader sr = new StreamReader(lokace))
+                {
+                    string radek;
+                    while ((radek = sr.ReadLine()) != null)
+                    {
+                        output += radek;
+                        Console.WriteLine(radek);
+                    }
+                }
+                return output;
+            }
+            //Zapis
+            else if (delej == "w")
+            {
+                using (StreamWriter sw = new StreamWriter(lokace))
+                {
+                    sw.WriteLine(data);
+                }
+            }
+            //Append
+            else if (delej == "a")
+            {
+                using (StreamWriter sw = new StreamWriter(lokace, true))
+                {
+                    sw.WriteLine(data);
+                }
+            }
+            return "";
+        }
+    }
+}
